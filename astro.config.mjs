0a51c3cd285a768aa7e2d726c8bf520ffbbd8273
felipeB21 <bolgarfelipe@gmail.com>
2024-04-09 23:49:11 -0300

    Added more pack

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from "astro/config";
+import db from "@astrojs/db";
 import vercel from "@astrojs/vercel/serverless";
 import auth from "auth-astro";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [auth(), tailwind(), react()],
+  integrations: [auth(), tailwind(), react(), db()],
   output: "server",
   adapter: vercel({
     webAnalytics: {
